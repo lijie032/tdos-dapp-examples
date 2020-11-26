5721644207e662a197b030726fe6b977a317f321
lijie032 <291788796@qq.com>
2020-11-26 15:22:08 +0800

    updates

--- src/main.js
+++ src/main.js
@@ -12,18 +12,38 @@ import 'element-ui/lib/theme-chalk/index.css';//样式文件一定要引入
 Vue.config.productionTip = false
 
 Vue.use(ElementUI);
+
+
 //定义全局临时变量
 var momentObj = {};
 Vue.prototype.getMainData = function () {
-	return momentObj
+  return momentObj
 }
-Vue.prototype.changeMainData = function (key,val) {
-	momentObj[key] = val
+Vue.prototype.changeMainData = function (key, val) {
+  momentObj[key] = val
 }
+
+
+var pubKey = "";
+var timer;
+timer = setInterval(function () {
+
+  var pk = document.getElementById('receiveP')
+  if (pk != null) {
+    pubKey = pk.innerHTML;
+  }
+
+}, 1000);
+Vue.prototype.getPK = function () {
+  return pubKey
+}
+
 //Use
 // that.changeMainData("name","gangYong")
 // that.changeMainData("old","88")
 // alert(JSON.stringify(that.getMainData()));
+
+
 
 
 Vue.use(toastRegistry)
