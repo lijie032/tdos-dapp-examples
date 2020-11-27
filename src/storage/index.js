f690b5a2b36c459ccb16a2a13b398ee9561395a1
lijie032 <291788796@qq.com>
2020-11-27 10:36:14 +0800

    公钥存缓存

---
+++ src/storage/index.js
@@ -0,0 +1,52 @@
+const env = process.env.NODE_ENV;
+
+var storage = {};
+
+if (env === 'development') {
+  storage.set = function (items) {
+  	// console.log(items)
+    var key, value;
+    for (let k in items) {
+      key = k;
+      value = items[k];
+      if (typeof value === 'object') {
+        try {
+          value = JSON.stringify(value);
+        } catch (e) {
+          console.log('本地存储报错',e);
+        }
+      }
+      localStorage.setItem(key, value)
+    }
+  };
+
+  storage.get = function (key) {
+    if (typeof key === 'string') {
+      var value = localStorage.getItem(key);
+      try {
+        value = JSON.parse(value)
+      } catch (e) {
+        console.log(e)
+      }
+
+      return Promise.resolve(value);
+    }
+  };
+}
+
+if (env === 'production') {
+  storage.set = function (items) {
+    
+    chrome.storage.local.set(items);
+  };
+
+  storage.get = function (key) {
+    return new Promise(function (resolve) {
+      chrome.storage.local.get(key, function (rs) {
+        resolve(rs[key])
+      });
+    })
+  }
+}
+
+export default storage;
